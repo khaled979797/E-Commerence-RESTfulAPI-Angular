1463476c2358a7bdd266dae5a1ea98ede5a54e9c
khaled979797 <khaled.alshreef25@gmail.com>
2024-10-17 03:32:31 +0300

    Adding the basket api and using redis to store the basket items

---
+++ APIs/E-Commerence.Infrastructure/Interfaces/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerence.Core.Entities;
+
+namespace E_Commerence.Infrastructure.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string id);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string id);
+    }
+}
